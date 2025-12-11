5cf4fd15b274a3daa83a120d97666e75e439aafc
Siyaman <siyaman.sadagopan@bytedance.com>
2025-12-11 12:13:29 -0800

    Remove mkcert
    Allow running dev command without root permissions

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import webspatial from '@webspatial/vite-plugin'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   base: '/',
   plugins: [
-    mkcert(),
     react({
       jsxImportSource: '@webspatial/react-sdk'  // Required for spatial components
     }),
@@ -19,5 +17,5 @@ export default defineConfig({
       inject: { data: { XR_ENV: process.env.XR_ENV } }
     })
   ],
-  server: { host: '0.0.0.0' }
+  server: { open: true }
 })
